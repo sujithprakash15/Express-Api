6be21814ea50600ffa9a437cf4b993c147a3a16c
sujithprakash15 <sujithprakash15@gmail.com>
2024-06-28 16:04:59 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/productmodel');
 
 const app=express();
 const port= 3000;
+
+
+//json middleware
+app.use(express.json());
 
 //routes
 app.get('/hai',(req,res)=>{
     res.send('Hellow');
 });
 
-app.listen(port);
+app.post('/Product',async(req,res)=>{
+try {
+    const product= await Product.create(req.body)
+    res.status(200).json(product);
+
+} catch (error) {
+    console.log(error.message)
+    res.status(500).json({message: error.message})   
+}
+})
+
+mongoose.connect('mongodb+srv://admin:admin@api.jry1zia.mongodb.net/nodeapi?retryWrites=true&w=majority&appName=api')
+.then(()=>
+{
+    console.log('connected to MongoDB')
+    app.listen(port, ()=>{
+        console.log("Node API app is running on port 3000");
+    });
+}).catch(()=>{
+    console.log(error) 
+})
---
+++ models/productmodel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const productschema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required:[true,"please enter a product name"]
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        image:{
+            type: String,
+            required: false,
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Product = mongoose.model('Product', productschema);
+module.exports=Product;
